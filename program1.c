7bf9897851717c91d1272bbe21286074c30d157e
Suhana  Suneer <suhanasuhaa24@gmail.com>
2025-10-21 12:04:55 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int a,b,s=0;
+printf("Enter the first number:");
+scanf("%d", &a);
+printf("Enter the second number:");
+scanf("%d", &b);
+s = a+b;
+printf("Sum of numbers is %d",s);
+}
